406514ece178a2ff0fbd572f06ad7f407222ab8b
Prem Prakash Shinde <shindeprem102@gmail.com>
2023-08-18 09:23:23 +0530

    Started with Google  Autentication Freature

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,26 @@
 import React, { useEffect } from "react";
-import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { getPosts } from "./actions/posts";
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom/cjs/react-router-dom.min";
+import { Container } from "@material-ui/core";
 
-import logo from "./images/logo.png";
-import Posts from "./components/Posts/Posts";
-import Form from "./components/Form/Form";
-import useStyles from "./styles";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
-  const classes = useStyles();
-  const [currentId, setCurrentId] = useState(0);
-
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
   return (
-    <Container maxWidth='lg'>
-      <AppBar className={classes.appBar} position='static' color='inherit'>
-        <Typography className={classes.heading} variant='h2' align='center'>
-          Memo_App
-        </Typography>
-        <img className={classes.image} src={logo} alt='memories' height='60' />
-      </AppBar>
-      <Grow in>
-        <Container>
-          <Grid
-            className={classes.mainContainer}
-            container
-            justifyContent='space-between'
-            alignItems='stretch'
-            spacing={3}
-          >
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxWidth='lg'>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/auth' exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
   );
 };
 
--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,7 @@
 export default (posts = [], action) => {
   switch (action.type) {
     case "DELETE":
-      return posts.filter((post) => post._id != action.payload);
+      return posts.filter((post) => post._id !== action.payload);
     case "UPDATE":
     case "LIKE":
       return posts.map((post) =>
